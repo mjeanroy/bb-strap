2612eed9af128e0328a3bf2cb9b8fb072e6f9fe0
mjeanroy <mickael.jeanroy@gmail.com>
2014-02-20 16:10:10 +0100

    add licence

--- src/bb-strap.js
+++ src/bb-strap.js
@@ -1,5 +1,25 @@
 /**
+ * The MIT License (MIT)
  *
+ * Copyright (c) 2014 Mickael Jeanroy <mickael.jeanroy@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 (function($, _, Backbone) {
@@ -117,7 +137,7 @@
 
     /** Clear all subscriptions. */
     clear: function() {
-      Backbone.Mediator.channels = {};
+      Backbone.Mediator.channels = {};
     },
 
     /** Subscribe to a channel */
@@ -210,8 +230,8 @@
       that.$body = $('body');
 
       that.$html
-        .addClass('js')
-        .removeClass('no-js');
+          .addClass('js')
+          .removeClass('no-js');
 
       var location = window.location;
       that.url = location.protocol + '//' + location.host + location.pathname;
@@ -713,7 +733,9 @@
 
       var fn = params;
       if (!_.isFunction(fn)) {
-        fn = function() { return params; };
+        fn = function() {
+          return params;
+        };
       }
 
       var added = [];
