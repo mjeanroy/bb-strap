56320e0f221fb5db5afd32cbfde02c469a55c144
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2014-05-21 21:20:18 +0200

    override views

--- src/view.js
+++ src/view.js
@@ -27,6 +27,138 @@ Backbone.View = (function(View) {
       constructor: function(options) {
         _.extend(this, options || {});
         View.apply(this, arguments);
+        this.setSubscriptions();
       }
     });
 })(Backbone.View);
+
+_.extend(Backbone.View.prototype, {
+
+  /**
+   * No-Op function that should be override when view is disposed
+   * to run custom logic.
+   */
+  onDispose: function() {},
+
+  /**
+   * Dispose view:
+   * - Trigger 'dispose' event.
+   * - Undelegate events.
+   * - Stop listening to everything.
+   * @return {Backbone.View} this.
+   */
+  dispose: function() {
+    this.trigger('dispose', this);
+    this.onDispose();
+    this.undelegateEvents();
+    this.stopListening();
+    this.unsetSubscriptions();
+    return this;
+  },
+
+  /**
+   * Remove view:
+   * - Dispose view.
+   * - Trigger 'close' event.
+   * - Remove view from DOM.
+   * - Destroy view.
+   * @return {Backbone.View} this.
+   */
+  remove: function() {
+    this.dispose();
+    this.trigger('close', this);
+    this.$el.remove();
+    this.$destroy();
+    return this;
+  },
+
+  /**
+   * Close view.
+   * Alias to remove function (to be backward compatible).
+   * @return {Backbone.View} this.
+   * @deprecated
+   */
+  close: function() {
+    return this.remove();
+  },
+
+  /**
+   * Clear view:
+   * - Dispose.
+   * - Trigger 'clear' event.
+   * - Clear view content from DOM.
+   * - Destroy view.
+   * @return {Backbone.View} this.
+   */
+  clear: function() {
+    this.dispose();
+    this.trigger('clear', this);
+    this.$el.empty();
+    this.$destroy();
+    return this;
+  },
+
+  /**
+   * Destroy view: remove everything attached to view object.
+   * @return {Backbone.View} this.
+   */
+  $destroy: function() {
+    for (var i in this) {
+      if (_.has(this, i)) {
+        this[i] = null;
+      }
+    }
+    return this;
+  },
+
+  /**
+   * Set subscriptions.
+   * @param {object=} subscriptions New subscriptions.
+   * @return {Backbone.View} this.
+   */
+  setSubscriptions: function(subscriptions) {
+    var callbacks = subscriptions || this.subscriptions;
+    if (callbacks && !_.isEmpty(callbacks)) {
+
+      // Ensure we don't subscribe twice
+      this.unsetSubscriptions(callbacks);
+
+      var iterator = function(subscription, channel) {
+        var once = !!subscription.once;
+
+        if (_.isString(subscription)) {
+          subscription = this[subscription];
+        }
+
+        Backbone.Mediator.sub(channel, subscription, this, once);
+      };
+
+      _.each(callbacks, iterator, this);
+    }
+
+    return this;
+  },
+
+  /**
+   * Remove subscriptions.
+   * @param {object=} subscriptions Subscriptions to remove.
+   * @return {Backbone.View} this.
+   */
+  unsetSubscriptions: function(subscriptions) {
+    var callbacks = subscriptions || this.subscriptions;
+
+    if (callbacks && !_.isEmpty(callbacks)) {
+
+      var iterator = function(subscription, channel) {
+        if (_.isString(subscription)) {
+          subscription = this[subscription];
+        }
+        Backbone.Mediator.unsubscribe(channel, subscription, this);
+      };
+
+      _.each(callbacks, iterator, this);
+    }
+
+    return this;
+  },
+});
--- test/view-Spec.js
+++ test/view-Spec.js
@@ -34,4 +34,147 @@ describe('View Spec', function() {
   	expect(view.foo).toBeDefined();
   });
 
+  describe('View Destruction', function() {
+    beforeEach(function() {
+      spyOn(Backbone.View.prototype, '$destroy').andCallThrough();
+      spyOn(Backbone.View.prototype, 'undelegateEvents').andCallThrough();
+      spyOn(Backbone.View.prototype, 'stopListening').andCallThrough();
+      spyOn(Backbone.View.prototype, 'onDispose').andCallThrough();
+      spyOn(Backbone.View.prototype, 'trigger').andCallThrough();
+      spyOn(Backbone.View.prototype, 'unsetSubscriptions').andCallThrough();
+
+      spyOn($.fn, 'empty');
+      spyOn($.fn, 'remove');
+
+      this.view = new Backbone.View();
+    });
+
+    it('should remove view', function() {
+      var $el = this.view.$el;
+
+      this.view.remove();
+
+      expect(this.view.onDispose).toHaveBeenCalled();
+      expect(this.view.trigger).toHaveBeenCalledWith('dispose', this.view);
+      expect(this.view.trigger).toHaveBeenCalledWith('close', this.view);
+      expect(this.view.undelegateEvents).toHaveBeenCalled();
+      expect(this.view.stopListening).toHaveBeenCalled();
+      expect(this.view.unsetSubscriptions).toHaveBeenCalled();
+      expect(this.view.$destroy).toHaveBeenCalled();
+      expect($el.remove).toHaveBeenCalled();
+    });
+
+    it('should close view', function() {
+      var $el = this.view.$el;
+
+      this.view.close();
+
+      expect(this.view.onDispose).toHaveBeenCalled();
+      expect(this.view.trigger).toHaveBeenCalledWith('dispose', this.view);
+      expect(this.view.trigger).toHaveBeenCalledWith('close', this.view);
+      expect(this.view.undelegateEvents).toHaveBeenCalled();
+      expect(this.view.stopListening).toHaveBeenCalled();
+      expect(this.view.unsetSubscriptions).toHaveBeenCalled();
+      expect(this.view.$destroy).toHaveBeenCalled();
+      expect($el.remove).toHaveBeenCalled();
+    });
+
+    it('should clear view', function() {
+      var $el = this.view.$el;
+
+      this.view.clear();
+
+      expect(this.view.onDispose).toHaveBeenCalled();
+      expect(this.view.trigger).toHaveBeenCalledWith('dispose', this.view);
+      expect(this.view.trigger).toHaveBeenCalledWith('clear', this.view);
+      expect($el.empty).toHaveBeenCalled();
+      expect(this.view.undelegateEvents).toHaveBeenCalled();
+      expect(this.view.stopListening).toHaveBeenCalled();
+      expect(this.view.unsetSubscriptions).toHaveBeenCalled();
+      expect(this.view.$destroy).toHaveBeenCalled();
+      expect($el.remove).not.toHaveBeenCalled();
+    });
+
+    it('should destroy view', function() {
+      this.view.model = new Backbone.Model();
+      this.view.collection = new Backbone.Collection();
+      this.view.xhr = jasmine.createSpyObj('xhr', ['abort']);
+
+      this.view.$destroy();
+
+      expect(this.view.model).toBe(null);
+      expect(this.view.collection).toBe(null);
+      expect(this.view.xhr).toBe(null);
+    });
+  });
+
+  describe('View Subscriptions', function() {
+    beforeEach(function() {
+      spyOn(Backbone.Mediator, 'sub').andCallThrough();
+      spyOn(Backbone.Mediator, 'unsubscribe').andCallThrough();
+
+      spyOn(Backbone.View.prototype, 'setSubscriptions').andCallThrough();
+      spyOn(Backbone.View.prototype, 'unsetSubscriptions').andCallThrough();
+
+      this.view = new Backbone.View();
+    });
+
+    it('should attach subscriptions on view creation', function() {
+      var view = new Backbone.View();
+
+      expect(view.setSubscriptions).toHaveBeenCalledWith();
+    });
+
+    it('should attach subscriptions on view creation', function() {
+      var fooFn = jasmine.createSpy('fooFn');
+      var barFn = jasmine.createSpy('barFn');
+
+      var CustomView = Backbone.View.extend({
+        subscriptions: {
+          'foo': 'fooFn',
+          'bar': 'barFn'
+        },
+
+        fooFn: fooFn,
+
+        barFn: barFn
+      });
+
+      var view = new CustomView();
+
+      expect(view.setSubscriptions).toHaveBeenCalledWith();
+      expect(Backbone.Mediator.sub).toHaveBeenCalledWith('foo', fooFn, view, false);
+      expect(Backbone.Mediator.sub).toHaveBeenCalledWith('bar', barFn, view, false);
+    });
+
+    it('should attach subscriptions', function() {
+      var fn1 = jasmine.createSpy('fn1');
+      var fn2 = jasmine.createSpy('fn2');
+
+      var subscriptions = {
+        foo: fn1,
+        bar: fn2
+      };
+
+      this.view.setSubscriptions(subscriptions);
+
+      expect(Backbone.Mediator.sub).toHaveBeenCalledWith('foo', fn1, this.view, false);
+      expect(Backbone.Mediator.sub).toHaveBeenCalledWith('bar', fn2, this.view, false);
+    });
+
+    it('should detach subscriptions', function() {
+      var fn1 = jasmine.createSpy('fn1');
+      var fn2 = jasmine.createSpy('fn2');
+
+      var subscriptions = {
+        foo: fn1,
+        bar: fn2
+      };
+
+      this.view.unsetSubscriptions(subscriptions);
+
+      expect(Backbone.Mediator.unsubscribe).toHaveBeenCalledWith('foo', fn1, this.view);
+      expect(Backbone.Mediator.unsubscribe).toHaveBeenCalledWith('bar', fn2, this.view);
+    });
+  });
 });
